113f9d4402666ea0bf3452f2ae6a896a67d3b37e
FinishV <finrichter@gmail.com>
2024-08-09 10:21:46 +0200

    feat: Klasse Game hinzugefügt

---
+++ Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPGGame
+{
+    public class Game
+    {
+        public void Start() 
+        {
+            Console.WriteLine("Willkommen!");
+            Console.WriteLine("Bevor du beginnen kannst musst du erst deinen Charakter erstellen.");
+        }
+    }
+}
